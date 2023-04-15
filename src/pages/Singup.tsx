17278b55cc87d9c642f9b07abeb8cb04e7c05d09
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-04-15 04:10:54 -0300

    feat: add Signup functionality

---
+++ src/pages/Singup.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+function Singup() {
+    const [username, setUsername] = useState('');
+    const [isDisabled, setIsDisabled] = useState(true);
+
+    const history = useHistory()
+
+    const validateButton = ({target: {value}}: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(value)
+        
+    }
+
+    useEffect(() => {
+        if (username.length === 0) {
+            return setIsDisabled(true);
+        }
+        return setIsDisabled(false)
+    }, [username])
+
+    return (
+        <div>
+            <h2>Welcome to CodeLeap network!</h2>
+            <label htmlFor="username">
+                <h4>Please enter your username</h4>
+                <input 
+                    type="text" 
+                    id='username' 
+                    alt='john doe'
+                    onChange={validateButton}
+                />
+                <button 
+                    disabled={isDisabled}
+                    type='button'
+                    onClick={() => history.push('/careers')}>
+                        ENTER
+                </button>
+            </label>
+        </div>
+    );
+}
+
+export default Singup;
