317c41fcf2ab60a74fbba5118a7717627f93792c
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-04-15 19:51:05 -0300

    test: add date test, api test and mocks

--- src/testes/Main.test.tsx
+++ src/testes/Main.test.tsx
@@ -3,8 +3,18 @@ import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
 import App from '../pages/Main';
 import renderWithRouter from './renderWithRouter';
+import MockAdapter from "axios-mock-adapter"
+import axios from "axios";
+import { firstGet } from './MainMock';
+
+const axiosMock = new MockAdapter(axios)
 
 describe('Testing Main', () => { 
+
+    beforeEach(() => {
+      jest.useFakeTimers('modern').setSystemTime(new Date("2023-04-15T03:54:53.795233Z"))
+    })
+
     it('test if you can\t create post without title and content', async () => {
      renderWithRouter(App)
      const createButton = await screen.findAllByRole('button', {name: /create/i})
@@ -27,7 +37,50 @@ describe('Testing Main', () => {
     expect(createButton).toBeDisabled()
    })
    
-//    it('test if you call api', () => {
-    
-//    }) 
+   it('test if you call api', async () => {
+    axiosMock.onGet('https://dev.codeleap.co.uk/careers/', firstGet)
+    expect("https://dev.codeleap.co.uk/careers/").toBeCalled()
+    const titlePost = await screen.findByRole('heading', {level: 2, name: "aasdsa" })
+    expect(titlePost).toBeInTheDocument()
+    const username = await screen.findByText("garrafa")
+    expect(username).toBeInTheDocument()
+    const content = await screen.findByText("sf")
+    expect(content).toBeInTheDocument()
+   }) 
+
+   it('test if receive hour with a year of difference', async () => {
+    axiosMock.onGet('https://dev.codeleap.co.uk/careers/', firstGet)
+    const timeText = await screen.findByText("1 year ago")
+    expect(timeText).toBeInTheDocument()
+   })
+
+   it('test if receive hour with a mouth of difference', async () => {
+    axiosMock.onGet('https://dev.codeleap.co.uk/careers/', firstGet)    
+    const timeText = await screen.findByText("1 mouth ago")
+    expect(timeText).toBeInTheDocument()
+   })
+
+   it('test if receive hour with a day of difference', async () => {
+    axiosMock.onGet('https://dev.codeleap.co.uk/careers/', firstGet)
+    const timeText = await screen.findByText("1 day ago")
+    expect(timeText).toBeInTheDocument()
+   })
+
+   it('test if receive hour with a hour of difference', async () => {
+    axiosMock.onGet('https://dev.codeleap.co.uk/careers/', firstGet)
+    const timeText = await screen.findByText("1 hour ago")
+    expect(timeText).toBeInTheDocument()
+   })
+
+   it('test if receive hour with a minute of difference', async () => {
+    axiosMock.onGet('https://dev.codeleap.co.uk/careers/', firstGet)
+    const timeText = await screen.findByText("27 minutes ago")
+    expect(timeText).toBeInTheDocument()
+   })
+
+   it('test if receive hour with a second of difference', async () => {
+    axiosMock.onGet('https://dev.codeleap.co.uk/careers/', firstGet)
+    const timeText = await screen.findByText("1 second ago")
+    expect(timeText).toBeInTheDocument()
+   })
 })
---
+++ src/testes/MainMock.ts
@@ -0,0 +1,49 @@
+export const firstGet = {
+    "count": 30,
+    "next": "https://dev.codeleap.co.uk/careers/?limit=10&offset=10",
+    "previous": null,
+    "results": [
+        {
+            "id": 6404,
+            "username": "garrafa",
+            "created_datetime": "2023-04-15T03:54:53.795233Z",
+            "title": "aasdsa",
+            "content": "sf"
+        },
+        {
+            "id": 6403,
+            "username": "garrafa",
+            "created_datetime": "2023-04-15T03:54:52.795233Z",
+            "title": "aasdsa",
+            "content": "sf"
+        },
+
+        {
+            "id": 6401,
+            "username": "Kim2",
+            "created_datetime": "2023-04-14T03:28:45.192432Z",
+            "title": "gfdg",
+            "content": "fdgfdg"
+        },
+        {
+            "id": 6400,
+            "username": "a",
+            "created_datetime": "2023-03-15T03:28:34.600566Z",
+            "title": "a",
+            "content": "a"
+        },
+        {
+            "id": 6397,
+            "username": "Pato",
+            "created_datetime": "2022-04-15T03:14:38.890376Z",
+            "title": "Dsf",
+            "content": "dsfsd"
+        },
+        {
+            "id": 6384,
+            "username": "John Doe",
+            "created_datetime": "2023-04-15T02:27:53.780647Z",
+            "title": "Teste.",
+            "content": "Teste."
+        },]
+    }
