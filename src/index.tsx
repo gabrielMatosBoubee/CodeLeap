b3718856fd17f98ca7773f7d4d5da12a3748103d
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-04-16 04:39:22 -0300

    feat: config index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import store from './redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={new QueryClient()} contextSharing={true}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
